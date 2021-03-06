86aefd47394a8ce9dace57e7a14f73b7f4dee8b3
cusoq <cusoq@mail.ru>
2021-03-06 12:06:23 +0300

    Fix the default status of slider and picture on click/esc close ones

--- js/effects.js
+++ js/effects.js
@@ -1,7 +1,8 @@
 /* global noUiSlider:readonly */
 import {
+  ESC_KEYCODE,
   SliderData
-} from './data.js';
+} from './data.js'
 
 const sliderElement = document.querySelector('.effect-level__slider');
 const scaleInputElement = document.querySelector('.scale__control--value');
@@ -15,6 +16,7 @@ const sepiaEffectInputElement = document.querySelector('#effect-sepia');
 const marvinEffectInputElement = document.querySelector('#effect-marvin');
 const phobosEffectInputElement = document.querySelector('#effect-phobos');
 const heatEffectInputElement = document.querySelector('#effect-heat');
+const uploadCancelButtonElement = document.querySelector('.img-upload__cancel');
 
 const createSlider = (min, max, start, step) => {
   noUiSlider.create(sliderElement, {
@@ -60,28 +62,34 @@ const groupeRepeatingEventListenerActions = (onCklickValueUpper, onCklickValueDo
   noneEffectInputElement.addEventListener('change', onChangeNoneEffectSetter);
 }
 
-const setEffect = () => {
-  const setPictureTransform = () => picturePreviewElement.style.transform = `scale(${parseInt(scaleInputElement.value) / 100})`
+const setSliderDefault = () => {
+  function setPictureTransform() {
+    return picturePreviewElement.style.transform = `scale(${parseInt(scaleInputElement.value) / 100})`;
+  }
   createSlider(SliderData.SCALE_RANGEMIN, SliderData.SCALE_RANGEMAX, SliderData.SCALE_START, SliderData.SCALE_STEP);
 
   const onUpdateSlider = (__, handle, unencoded) => {
     scaleInputElement.value = `${unencoded[handle]}%`;
     setPictureTransform();
-  }
+  };
   sliderElement.noUiSlider.on('update', onUpdateSlider);
 
   const onCklickValueUpper = () => {
     scaleInputElement.value = `${parseInt(scaleInputElement.value) + SliderData.SCALE_STEP}%`;
     setScaleLevelByButtons(setPictureTransform());
-  }
+  };
 
   const onCklickValueDowner = () => {
     scaleInputElement.value = `${parseInt(scaleInputElement.value) - SliderData.SCALE_STEP}%`;
     setScaleLevelByButtons(setPictureTransform());
-  }
+  };
+  return { onCklickValueUpper, onCklickValueDowner };
+}
 
-  const onChangeNoneEffectSetter = () => {
-    isSliderDestroy();
+
+const setEffect = () => {
+  const { onCklickValueUpper, onCklickValueDowner } = setSliderDefault();
+  const setPictureDefault = () => {
     picturePreviewElement.style.filter = 'none';
     picturePreviewElement.className = 'img-upload__preview';
     scaleControlBiggerElement.removeEventListener('click', onCklickValueUpper);
@@ -92,6 +100,12 @@ const setEffect = () => {
     marvinEffectInputElement.addEventListener('change', onChangeMarvinEffectSetter);
     phobosEffectInputElement.addEventListener('change', onChangePhobosEffectSetter);
     heatEffectInputElement.addEventListener('change', onChangeHeatEffectSetter);
+  }
+
+  const onChangeNoneEffectSetter = () => {
+    isSliderDestroy();
+    setSliderDefault();
+    setPictureDefault();
   };
 
   const onChangeChromeEffectSetter = () => {
@@ -157,6 +171,22 @@ const setEffect = () => {
     groupeRepeatingEventListenerActions(onCklickValueUpper, onCklickValueDowner, onChangeNoneEffectSetter);
   };
 
+  const onClickCloser = () => {
+    isSliderDestroy();
+    setSliderDefault();
+    setPictureDefault();
+  };
+
+  const onEscCloser = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      evt.preventDefault();
+      window.removeEventListener('keydown', onEscCloser);
+      isSliderDestroy();
+      setSliderDefault();
+      setPictureDefault();
+    }
+  };
+
   noneEffectInputElement.addEventListener('change', onChangeNoneEffectSetter);
   chromeEffectInputElement.addEventListener('change', onChangeChromeEffectSetter);
   sepiaEffectInputElement.addEventListener('change', onChangeSepiaEffectSetter);
@@ -165,7 +195,11 @@ const setEffect = () => {
   heatEffectInputElement.addEventListener('change', onChangeHeatEffectSetter);
   scaleControlBiggerElement.addEventListener('click', onCklickValueUpper);
   scaleControlSmallerElement.addEventListener('click', onCklickValueDowner);
+  uploadCancelButtonElement.addEventListener('click', onClickCloser);
+  document.addEventListener('keydown', onEscCloser);
 }
 export {
   setEffect
 };
+
+
